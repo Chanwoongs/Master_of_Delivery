303a0307193c55f0f240cb06a73963537b5abb75
Chanwoongs <tkfkdcksdnd@daum.net>
2021-12-05 02:27:45 +0900

    추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
-{ 
+{
     // 남은 시간 UI
     public Slider timeSlider;
     // 남은 시간 Text
@@ -23,14 +23,9 @@ public class GameManager : MonoBehaviour
     int currentOil;
     bool isInCar = false;
 
-    // 남은 음식 UI
-    public Image food1;
-    public Image food2;
-    public Image food3;
-    public Image food4;
-    public Image food5;
-    public Image food6;
-    public Image food7;
+    // 남은 사탕 UI
+    public Image candy1;
+    public Image candy2;
 
     // 배달 완료 Flags 
     // -1 -> 초기상태, 0 -> 배달 X, 1 -> 배달 O
@@ -41,7 +36,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] public int isDeliverd5;
     [SerializeField] public int isDeliverd6;
     [SerializeField] public int isDeliverd7;
-    
+
     // 배달지용 난수 생성
     private int randNum1;
     private int randNum2;
@@ -54,18 +49,15 @@ public class GameManager : MonoBehaviour
     public GameObject playerCamera;
     public GameObject secondLand;
     public GameObject secondLandCamera;
-
-    private GameObject player;
+    GameObject escMenu;
     Vector3 target;
     public UnityEvent toSecondLand;
 
     CarController cc;
-    
+    public CameraMove cam;
 
     private void Start()
     {
-        player = GameObject.Find("Player");
-
         isDeliverd1 = 0;
         isDeliverd2 = -1;
         isDeliverd3 = -1;
@@ -73,12 +65,6 @@ public class GameManager : MonoBehaviour
         isDeliverd5 = -1;
         isDeliverd6 = -1;
         isDeliverd7 = -1;
-
-        // 2번째 배달 지역 음식 UI 끄기
-        food4.gameObject.SetActive(false);
-        food5.gameObject.SetActive(false);
-        food6.gameObject.SetActive(false);
-        food7.gameObject.SetActive(false);
 
         // 초기 시간 설정
         time = 18000.0f;
@@ -104,6 +90,7 @@ public class GameManager : MonoBehaviour
         // 첫번째 영역 배달지 활성화
         GameObject.Find("Section1").transform.GetChild(randNum1).gameObject.SetActive(true);
         cc = GameObject.Find("DeliveryCar").GetComponent<CarController>();
+        escMenu = GameObject.Find("UICanvas").transform.GetChild(4).gameObject;
     }
 
     void Update()
@@ -112,10 +99,12 @@ public class GameManager : MonoBehaviour
         updateTime();
         updateDestination();
 
-        if(isInCar)
+        if (isInCar)
         {
             UpdateOilgaGage();
         }
+
+        UpdateEscKey();
     }
 
     // 배달지 업데이트
@@ -123,13 +112,11 @@ public class GameManager : MonoBehaviour
     {
         if (isDeliverd2 == 0)
         {
-            GameObject.Find("Section2").transform.GetChild(randNum2).gameObject.SetActive(true);
-            food1.gameObject.SetActive(false);
+            activate2ndDestination();
         }
         if (isDeliverd3 == 0)
         {
-            GameObject.Find("Section3").transform.GetChild(randNum3).gameObject.SetActive(true);
-            food2.gameObject.SetActive(false);
+            activate3rdDestination();
         }
         if (isDeliverd4 == 0)
         {
@@ -139,28 +126,51 @@ public class GameManager : MonoBehaviour
                 toSecondLand.Invoke();
             }
             toSecondLand = null;
-            GameObject.Find("Section4").transform.GetChild(randNum4).gameObject.SetActive(true);
-            food3.gameObject.SetActive(false);
-            food4.gameObject.SetActive(true);
-            food5.gameObject.SetActive(true);
-            food6.gameObject.SetActive(true);
-            food7.gameObject.SetActive(true);
+            activate4thDestination();
         }
         if (isDeliverd5 == 0)
         {
-            GameObject.Find("Section6").transform.GetChild(randNum6).gameObject.SetActive(true);
-            food4.gameObject.SetActive(false);
+            activate5thDestination();
         }
         if (isDeliverd6 == 0)
         {
-            GameObject.Find("Section5").transform.GetChild(randNum5).gameObject.SetActive(true);
-            food6.gameObject.SetActive(false);
+            activate6thDestination();
         }
         if (isDeliverd7 == 0)
         {
-            GameObject.Find("Section7").transform.GetChild(randNum7).gameObject.SetActive(true);
-            food5.gameObject.SetActive(false);
+            activate7thDestination();
         }
+    }
+
+    // 두번째 배달지 활성화
+    private void activate2ndDestination()
+    {
+        GameObject.Find("Section2").transform.GetChild(randNum2).gameObject.SetActive(true);
+    }
+    // 세번째 배달지 설정
+    private void activate3rdDestination()
+    {
+        GameObject.Find("Section3").transform.GetChild(randNum3).gameObject.SetActive(true);
+    }
+    // 네번째 배달지 설정 (햄버거 배달지)
+    private void activate4thDestination()
+    {
+        GameObject.Find("Section4").transform.GetChild(randNum4).gameObject.SetActive(true);
+    }
+    // 다섯번째 배달지 설정 (햄버거 배달지)
+    private void activate5thDestination()
+    {
+        GameObject.Find("Section5").transform.GetChild(randNum5).gameObject.SetActive(true);
+    }
+    // 세번째 배달지 설정 (사탕 배달지)
+    private void activate6thDestination()
+    {
+        GameObject.Find("Section6").transform.GetChild(randNum6).gameObject.SetActive(true);
+    }
+    // 세번째 배달지 설정 (사탕 배달지)
+    private void activate7thDestination()
+    {
+        GameObject.Find("Section7").transform.GetChild(randNum7).gameObject.SetActive(true);
     }
 
     private void gameClear()
@@ -192,13 +202,13 @@ public class GameManager : MonoBehaviour
     public void UpdateOilgaGage()
     {
         remainingOil -= 1.2f * Time.deltaTime;
-        oilSlider.value = remainingOil;
         currentOil = (int)remainingOil;
+        oilSlider.value = remainingOil;
         oilTxt.text = currentOil.ToString() + "%";
 
         if (remainingOil < 0f)
         {
-            remainingOil = 0; 
+            remainingOil = 0;
             cc.setMotorForce(0);  // 자동차의 힘을 0으로
             cc.rb.velocity *= 0.1f;
         }
@@ -219,13 +229,11 @@ public class GameManager : MonoBehaviour
         secondLand.transform.position = new Vector3(secondLand.transform.position.x, -40f, secondLand.transform.position.z);
         playerCamera.SetActive(false);
         secondLandCamera.SetActive(true);
-        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1000, player.transform.position.z);
 
         Invoke("changeCameraToPlayer", 5.0f);
     }
     private void changeCameraToPlayer()
     {
-        player.transform.position = new Vector3(player.transform.position.x, 0, player.transform.position.z);
         playerCamera.SetActive(true);
         secondLandCamera.SetActive(false);
     }
@@ -238,7 +246,37 @@ public class GameManager : MonoBehaviour
     public void setRemainingOil(float value)
     {
         remainingOil += value;
+
         if (remainingOil > oilSlider.maxValue)
-            remainingOil = oilSlider.maxValue +1;
+            remainingOil = oilSlider.maxValue + 1;
+    }
+
+    void UpdateEscKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!escMenu.activeSelf)
+            {
+                escMenu.SetActive(true);
+                cam.setIsAction(false);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                escMenu.SetActive(false);
+                cam.setIsAction(true);
+                Time.timeScale = 1;
+            }
+
+        }
+    }
+
+    public void ReStart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void GameQuit()
+    {
+        Application.Quit();
     }
 }
